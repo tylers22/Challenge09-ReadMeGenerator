115a10aeb3b9aeb5d8ec4d615e9c8864c4d9bba6
tylers22 <tylersharp1@gmail.com>
2021-10-29 16:46:06 -0700

    inquirer install and questions created

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer"); 
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the tilte of your project'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for you project?',
+        choices: ['MIT', 'None'],
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(response => {
+        console.log("user Input is ", response); 
+        //generate the readme.md file 
+    })
+}
 
 // Function call to initialize app
 init();
